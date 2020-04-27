e4b8663bee1b0f77cc00c131e2481518a0dd0bae
luzcry <mcrodriguezt618@gmail.com>
2020-04-27 16:11:59 -0500

    arrays

---
+++ __tests__/arrays.test.js
@@ -0,0 +1,16 @@
+import {
+    arrayFruits,
+    arrayColors
+} from '../arrays';
+
+describe('comprobaremos que existe un elemento', () => {
+    test('¿tiene una banana?', () => {
+        expect(arrayFruits()).toContain('banana');
+    });
+    test('no contiene un platano', () => {
+        expect(arrayFruits()).not.toContain('platano');
+    });
+    test('comprobar el tamaño de un arreglo', () => {
+        expect(arrayFruits()).toHaveLength(5);
+    });
+});
