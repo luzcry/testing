e2043e3541ee71055ffc589c7202f1054612d4e8
luzcry <mcrodriguezt618@gmail.com>
2020-04-27 23:06:10 -0500

    peticion a apis

---
+++ promise.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const getDataFromApi = (url) => {
+    return axios.get(url)
+        .then(({
+            data
+        }) => {
+            return data;
+        });
+};
